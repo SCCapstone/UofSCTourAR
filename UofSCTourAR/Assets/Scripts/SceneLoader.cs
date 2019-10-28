b2bc917f9df6283e7fac5a8d0135487c6c138dae
Ayla Nickerson <aylanickerson@Aylas-MacBook-Air.local>
2019-10-27 20:44:55 -0400

    added scene switching script

---
+++ UofSCTourAR/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadSceneOnClick(string sceneName) //scene name param
+    {
+        SceneManager.LoadScene(sceneName); //will switch to scene on click
+    } 
+}
