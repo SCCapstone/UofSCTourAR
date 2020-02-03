bd6ee37de63ac6cf626d5c269cd05e6fbc5659a3
colbyhill21 <acehill21@ymail.com>
2020-02-02 21:01:42 -0500

    Added four unit tests to the project for M13

---
+++ TourAR/Assets/Editor/sceneLoaderTest.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+
+public class sceneLoaderTest : MonoBehaviour
+{
+    [Test]
+    public void searchForTourHistoryScene_Test()
+    {
+        var sceneLoader = new SearchSceneLoader();
+
+        var sceneExists = sceneLoader.SceneExists("TourHistory");
+        Assert.AreEqual(sceneExists, true);
+    }
+    [Test]
+    public void searchForMapScene_Test()
+    {
+        var sceneLoader = new SearchSceneLoader();
+
+        var sceneExists = sceneLoader.SceneExists("Map");
+        Assert.AreEqual(sceneExists, true);
+    }
+
+    [Test]
+    public void searchInvalidScene_Test()
+    {
+        var sceneLoader = new SearchSceneLoader();
+
+        var sceneExists = sceneLoader.SceneExists("blehehahfhahah");
+        Assert.AreEqual(sceneExists, false);
+    }
+
+    [Test]
+    public void searchCaseInsensitive_Test()
+    {
+        var sceneLoader = new SearchSceneLoader();
+
+        var sceneExists = sceneLoader.SceneExists("tourhistory");
+        Assert.AreEqual(sceneExists, false);
+    }
+
+}
