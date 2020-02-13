e4b320bd8f324146d2a965d22db9424db42d5e3b
colbyhill21 <acehill21@ymail.com>
2020-02-13 15:59:54 -0500

    Adding File IO capabilities

--- TourAR/Assets/Editor/sceneLoaderTest.cs
+++ TourAR/Assets/Editor/sceneLoaderTest.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using NUnit.Framework;
+using System.IO;
+using System;
 
 public class sceneLoaderTest : MonoBehaviour
 {
@@ -38,6 +40,81 @@ public class sceneLoaderTest : MonoBehaviour
 
         var sceneExists = sceneLoader.SceneExists("tourhistory");
         Assert.AreEqual(sceneExists, false);
+    }
+
+    [Test]
+    public void testRead()
+    {
+        var readPath = Application.dataPath + "/Test.txt";
+        Assert.AreEqual(readFile(readPath), "thisisatest");
+    }
+
+    string readFile(string filePath) 
+    {
+        StreamReader sReader = new StreamReader(filePath);
+        string toReturn = "";
+        while(!sReader.EndOfStream) 
+        {
+            string line = sReader.ReadLine();
+            toReturn += line;
+        }
+
+        sReader.Close();
+        return toReturn;
+    }
+
+    [Test]
+    public void testWrite()
+    {
+        var writePath = Application.dataPath + "/TestWrite.txt";
+        List<string> myFile = new List<string>();
+        myFile.Add("This should work \nalso noice");
+        WriteFile(writePath, myFile);
+    }
+    
+    void WriteFile(string filePath, List<string> fileToWrite) 
+    {
+        StreamWriter sWriter;
+
+        if(!File.Exists(filePath)) 
+        {
+            sWriter = File.CreateText(Application.dataPath + "/Test.txt");
+        }
+        else 
+        {
+            sWriter = File.CreateText(filePath);
+        }
+
+        for(int i = 0; i < fileToWrite.Count; i++) 
+        {
+            sWriter.WriteLine(fileToWrite[i]);
+        }
+        sWriter.Close();
+    }
+
+    [Test]
+    public void testReadJSON()
+    {
+        var writePath = Application.dataPath + "/TestWrite.txt";
+        List<string> myFile = new List<string>();
+        myFile.Add("This should work \nalso noice");
+        WriteFile(writePath, myFile);
+    }
+
+    public void LoadJson()
+    {
+        using (StreamReader r = new StreamReader("file.json"))
+        {
+            string json = r.ReadToEnd();
+            List<Item> items = JsonConvert.DeserializeObject<List<Item>>(json);
+        }
+    }
+
+    public class TourStop
+    {
+        public string name;
+        public boolean isVisited;
+        public string description;
     }
 
 }
