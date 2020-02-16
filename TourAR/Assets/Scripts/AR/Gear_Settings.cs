b48d0b4971a8f301a7c9f631f8b7dc5e02c5dd6c
Wade Lewis <wlewis830@gmail.com>
2020-02-16 17:40:55 -0500

    AR 2.0
    Added:
    -Placement Indicator
    -UofSC Cube w/button

---
+++ TourAR/Assets/Scripts/AR/Gear_Settings.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gear_Settings : MonoBehaviour
+{
+    enum type {Button, Gear};
+    [SerializeField] type objtype;
+
+    public static int size = 0;
+    public GameObject[] settings = new GameObject[size];
+
+    [SerializeField] GameObject boxTop;
+    Animator boxTopAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ToggleSettings() {
+
+      switch(objtype) {
+        case type.Button:
+          boxTopAnimator = boxTop.GetComponent<Animator>();
+
+
+
+
+          break;
+        case type.Gear:
+          foreach (GameObject set in settings) {
+            set.SetActive(!set.activeInHierarchy);
+          }
+          break;
+
+        default:
+          break;
+      }
+
+
+    }
+}
