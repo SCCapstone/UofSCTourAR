eb5189f76fd6260f32810012ad6d0da784eb6936
Wade Lewis <wlewis830@gmail.com>
2020-02-18 21:29:03 -0500

    More AR Tweaks
    Fixed text to AR obj

--- TourAR/Assets/Scripts/loadTourStops.cs
+++ TourAR/Assets/Scripts/loadTourStops.cs
@@ -3,14 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using Newtonsoft.Json;
 using System.IO;
+using UnityEngine.UI;
 
 public class loadTourStops : MonoBehaviour
 {
     private List<TourStop> tourStops;
+    public Text stopTitle;
+    public Text data;
+    public Dropdown selection;
+    //private int prevDropVal=-1;
 
-    public loadTourStops()
+
+    /*public loadTourStops()
     {
-        loadTheTourStops();
+        //loadTheTourStops();
+        //sendToAR();
+    }*/
+
+    void Start() {
+
+      /*
+      selection = GameObject.Find("ChooseStop").GetComponent<Dropdown>();
+
+      if (selection == null) {
+        Debug.Log("I have a bad feeling about this...");
+      }
+      Debug.Log(selection.value);
+      prevDropVal = selection.value;
+      */
+
+      loadTheTourStops();
+      sendToAR();
+    }
+
+    void Update() {
+      //if (prevDropVal != selection.value)
+        //sendToAR();
     }
 
     public class TourStop
@@ -30,18 +58,36 @@ public class loadTourStops : MonoBehaviour
         public string closestPOI;
     }
 
+    private void sendToAR() {
+      Debug.Log("sendToAR");
+      Debug.Log(selection.options[selection.value].text);
+      Debug.Log(selection.captionText.text);
+      stopTitle.text = this.getTitleForTourStop("Rutledge");
+      data.text = this.getMetadataForTourStop("Rutledge");
+    }
+
+
     private void loadTheTourStops()
     {
         /*
             Application.dataPath is required because it is the only way to ensure that we are accessing our assets folder,
             on both desktop runs and mobile.
         */
-        string filePath = Application.dataPath + "/tourStops.json";
+        //Debug.Log("dataPath: "+Application.dataPath);
+        //string filePath = Application.dataPath + "Resources/JSON/tourStops.json";
+
+        string json = Resources.Load<TextAsset>("JSON/tourStops").text;
+        Debug.Log("JSON DATA:  "+ json);
+        tourStops = JsonConvert.DeserializeObject<List<TourStop>>(json);
+
+
+        /*
         using (StreamReader r = new StreamReader(filePath))
         {
             string json = r.ReadToEnd();
             tourStops = JsonConvert.DeserializeObject<List<TourStop>>(json);
         }
+        */
     }
 
     public List<TourStop> getTourStops()
@@ -49,8 +95,21 @@ public class loadTourStops : MonoBehaviour
         return tourStops;
     }
 
+    public string getTitleForTourStop(string bID) {
+        //title
+        string retVal = "";
+        for(int i = 0; i < tourStops.Count; i++)
+        {
+            if(tourStops[i].buildingID.Equals(bID))
+            {
+                retVal += tourStops[i].name;
+            }
+        }
+        return retVal;
+    }
+
     public string getMetadataForTourStop(string bID) {
-        //constructed:
+        // constructed:
         // Location:
         // description:
         string retVal = "";
@@ -58,9 +117,9 @@ public class loadTourStops : MonoBehaviour
         {
             if(tourStops[i].buildingID.Equals(bID))
             {
-                retVal += "<b>Contstructed: </b>" + tourStops[i].year;
-                retVal += "<br>Location: " + tourStops[i].location;
-                retVal += "\nDescription: " + tourStops[i].description;
+                retVal += "<b>Contstructed:</b> " + tourStops[i].year;
+                retVal += "\n<b>Location:</b> " + tourStops[i].location;
+                retVal += "\n<b>Description:</b> " + tourStops[i].description;
             }
         }
         return retVal;
@@ -68,9 +127,9 @@ public class loadTourStops : MonoBehaviour
 
     public string[] getAllDataForTourStop(string bID) {
         string[] retVal = new string[2];
-        for(int i = 0; i < tourStops.Count; i++) 
+        for(int i = 0; i < tourStops.Count; i++)
         {
-            if(tourStops[i].buildingID.Equals(bID)) 
+            if(tourStops[i].buildingID.Equals(bID))
             {
                 retVal[0] = tourStops[i].name;
                 retVal[1] = tourStops[i].description;
