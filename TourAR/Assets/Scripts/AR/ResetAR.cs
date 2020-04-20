af6bb6d9b2996cfeafb5d25cc01629be5e337ccb
Wade Lewis <wlewis830@gmail.com>
2020-04-20 16:44:56 -0400

    Reset Works

--- TourAR/Assets/Scripts/AR/ResetAR.cs
+++ TourAR/Assets/Scripts/AR/ResetAR.cs
@@ -8,8 +8,22 @@ public class ResetAR : MonoBehaviour
    
 
     public void Reset() {
-        GameObject uofscCube = GameObject.Find("UofSC Cube");
-        Destroy(uofscCube);
+
+        GameObject[] objectsToDelete;
+
+        objectsToDelete = GameObject.FindGameObjectsWithTag("ARObject");
+
+        foreach (GameObject obj in objectsToDelete) {
+            Debug.Log("Destroyed: " + obj.name);
+            Destroy(obj);
+        }
+
+        objectsToDelete = GameObject.FindGameObjectsWithTag("ARObject");
+
+        Debug.Log("Objects were destroyed");
+        foreach(GameObject obj in objectsToDelete) {
+            Debug.Log(obj.name);
+        }
 
         placementIndicator.SetActive(true);
     }
