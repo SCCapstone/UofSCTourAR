29871eed640ef3901e6bda08928ee12fb2f33dd5
Wade Lewis <wlewis830@gmail.com>
2020-04-22 15:14:39 -0400

    Fixed TourHistory Text, menu is smoother

---
+++ TourAR/Assets/Scripts/MenuAnimType.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuAnimType : MonoBehaviour
+{   
+
+
+    public static bool noMenu=true;
+    [SerializeField] bool dontInitialize = false;
+
+    Animator menu;
+
+    [SerializeField] RuntimeAnimatorController menuStart, menuNoStart;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (!dontInitialize) {
+            menu = gameObject.GetComponent<Animator>();
+            if (noMenu) {
+                Debug.Log("noMenu");
+                menu.runtimeAnimatorController = menuNoStart;
+                noMenu = false;
+            } else {
+                menu.runtimeAnimatorController = menuStart;
+            }
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setNoMenu() {
+        noMenu = true;
+    }
+}
