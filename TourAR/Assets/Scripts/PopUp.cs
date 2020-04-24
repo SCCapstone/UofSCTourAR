b0f636b42f0f71c48e2236b91c08947beaa0941d
Wade Lewis <wlewis830@gmail.com>
2020-04-24 17:45:20 -0400

    Fixes #115 , new popup and new font for achievements

--- TourAR/Assets/Scripts/PopUp.cs
+++ TourAR/Assets/Scripts/PopUp.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 //Popup function to make a panel of information popup onclick
 public class PopUp : MonoBehaviour
@@ -8,6 +9,7 @@ public class PopUp : MonoBehaviour
     public GameObject Panel;
 
 
+    [SerializeField] Image iconObj;
 
     public void OpenPopup()
     {
@@ -19,7 +21,6 @@ public class PopUp : MonoBehaviour
 
             Panel.SetActive(!isActive);
         }
-        Debug.Log("Open");
     }
 
     public void ClosePopup()
@@ -27,6 +28,17 @@ public class PopUp : MonoBehaviour
         if (Panel != null)
         {
             Panel.SetActive(false);
+        }
+    }
+
+    public void lockedPopup()
+    {
+
+        if (iconObj.sprite.name == "lock" && Panel != null)
+        {
+            bool isActive = Panel.activeSelf;
+
+            Panel.SetActive(!isActive);
         }
     }
 }
